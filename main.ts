733395bc56b05cd61a359b29f07ac0e2048db8e5
ian9487 <76760128+ian9487@users.noreply.github.com>
2021-01-04 20:30:59 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+effects.bubbles.startScreenEffect()
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . c c c c c 
+    . . . . . . . . . c c 7 7 7 6 c 
+    . . . . . . . . c c 7 7 7 c c . 
+    . . . . . . . . c 6 7 7 c . . . 
+    . . . . . . . . c 6 6 6 c . . . 
+    . . . . . . . . c 6 6 6 c c . . 
+    . . . c c c c c c c 6 6 6 c c . 
+    . . c 6 7 7 7 7 6 c c 6 6 6 c . 
+    . c 7 7 7 7 7 7 7 7 c 6 6 6 c c 
+    c 6 7 7 7 7 7 7 7 7 6 c 6 6 6 c 
+    c 7 c 6 6 6 6 c 7 7 7 c 6 6 6 c 
+    f 7 c c 6 6 c c 7 7 7 f 6 6 6 c 
+    f 7 6 f 6 6 f 6 7 7 7 f 6 6 6 c 
+    . f 7 7 7 7 7 7 7 7 6 f 6 6 c . 
+    . c 1 c f f 1 c 7 6 f 6 6 c c . 
+    . c c c c c c c c c c c c . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
